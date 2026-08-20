aa0a513ab60a01ca434b3609ed540d76058a9f3d
theSUN <sandesh.805421@ac.tu.edu.np>
2026-08-20 21:01:32 +0545

    Removed ADMIN PANEL button from homepage

--- includes/header.php
+++ includes/header.php
@@ -71,9 +71,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     <a href="login.php">Login</a>
                     <a href="register.php">Register</a>
                 <?php endif; ?>
-                <?php if (isAdminLoggedIn()): ?>
-                    <a href="admin/dashboard.php" class="admin-top-link">Admin Panel</a>
-                <?php endif; ?>
+
             </div>
         </div>
     </div>
